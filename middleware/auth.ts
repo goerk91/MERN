6a0892164f204822bea6e91e719305803a1323d9
Metin Görken <metin.goerken@adesso.de>
2021-07-15 17:39:39 +0200

    new middleware

---
+++ middleware/auth.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+function auth(_req: Request, _res: Response, _next: NextFunction) {
+  try {
+    const token = _req.cookies.token;
+    const env = process.env["JWT_SECRET"] || "";
+
+    if (!token)
+      return _res.status(401).json({
+        errorMessage: "Unathorized",
+      });
+
+    const verified = jwt.verify(token, env);
+    _req.user = verified;
+    return _next();
+  } catch (err) {
+    console.error(err);
+    return _res.status(401).json({
+      errorMessage: "Unathorized",
+    });
+  }
+}
+
+export default auth;
