5d6978466f2efdc9a8671f76d8a2f2981bf3fd29
Metin Görken <metin.goerken@adesso.de>
2021-07-15 10:13:29 +0200

    finished logout

--- dist/routers/userRouter.js
+++ dist/routers/userRouter.js
@@ -7,9 +7,11 @@ exports.router = void 0;
 const express_1 = __importDefault(require("express"));
 const userModel_1 = require("../models/userModel");
 const bcrypt_1 = __importDefault(require("bcrypt"));
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 exports.router = express_1.default.Router();
 exports.router.post("/", async (_req, _res) => {
     try {
+        let env = process.env["JWT_SECRET"] || "";
         const { email, password, passwordVerify } = _req.body;
         if (!email || !password || !passwordVerify) {
             return _res
@@ -39,12 +41,57 @@ exports.router.post("/", async (_req, _res) => {
             passwordHash: passwordHash,
         });
         const savedUser = await newUser.save();
-        console.log(savedUser);
-        return _res.send("User saved");
+        const token = jsonwebtoken_1.default.sign({
+            user: savedUser._id,
+        }, env);
+        return _res
+            .cookie("token", token, {
+            httpOnly: true,
+        })
+            .send("user saved!");
     }
     catch (err) {
         console.error(err);
         return _res.status(500).send();
     }
+});
+exports.router.post("/login", async (_req, _res) => {
+    try {
+        const { email, password } = _req.body;
+        let env = process.env["JWT_SECRET"] || "";
+        if (!email || !password) {
+            return _res
+                .status(400)
+                .json({ errorMessage: "Please enter all required fields!" });
+        }
+        const existingUser = await userModel_1.User.findOne({ email });
+        if (!existingUser) {
+            return _res.status(401).json({ errorMessage: "Wrong email or password" });
+        }
+        const passwordCorrect = await bcrypt_1.default.compare(password, existingUser.passwordHash);
+        if (!passwordCorrect) {
+            return _res.status(401).json({ errorMessage: "Wrong email or password" });
+        }
+        const token = jsonwebtoken_1.default.sign({
+            user: existingUser._id,
+        }, env);
+        return _res
+            .cookie("token", token, {
+            httpOnly: true,
+        })
+            .send("Login was successful!");
+    }
+    catch (err) {
+        console.error(err);
+        return _res.status(500).send("Wtf");
+    }
+});
+exports.router.get("/logout", (_req, _res) => {
+    _res
+        .cookie("token", "", {
+        httpOnly: true,
+        expires: new Date(0),
+    })
+        .send();
 });
 //# sourceMappingURL=userRouter.js.map
--- routers/userRouter.ts
+++ routers/userRouter.ts
@@ -1,11 +1,14 @@
 import express, { Request, Response } from "express";
 import { User } from "../models/userModel";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const router = express.Router();
 
+// register
 router.post("/", async (_req: Request, _res: Response) => {
   try {
+    let env = process.env["JWT_SECRET"] || "";
     const { email, password, passwordVerify } = _req.body;
 
     if (!email || !password || !passwordVerify) {
@@ -35,19 +38,89 @@ router.post("/", async (_req: Request, _res: Response) => {
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
 
-    //save a new user to DB
+    // save a new user to DB
     const newUser = new User({
       email: email,
       passwordHash: passwordHash,
     });
 
     const savedUser = await newUser.save();
-    console.log(savedUser);
 
-    // log the user in
-    return _res.send("User saved");
+    // sign the token
+    const token = jwt.sign(
+      {
+        user: savedUser._id,
+      },
+      env
+    );
+    // console.log(token);
+
+    // send the token in a HTTP-cookie
+    return _res
+      .cookie("token", token, {
+        httpOnly: true,
+      })
+      .send("user saved!");
   } catch (err) {
     console.error(err);
     return _res.status(500).send();
   }
+});
+
+// log in
+router.post("/login", async (_req: Request, _res: Response) => {
+  try {
+    const { email, password } = _req.body;
+    let env = process.env["JWT_SECRET"] || "";
+
+    //validate
+    if (!email || !password) {
+      return _res
+        .status(400)
+        .json({ errorMessage: "Please enter all required fields!" });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      return _res.status(401).json({ errorMessage: "Wrong email or password" });
+    }
+
+    const passwordCorrect = await bcrypt.compare(
+      password,
+      existingUser.passwordHash
+    );
+
+    if (!passwordCorrect) {
+      return _res.status(401).json({ errorMessage: "Wrong email or password" });
+    }
+
+    // sign the token
+    const token = jwt.sign(
+      {
+        user: existingUser._id,
+      },
+      env
+    );
+    // console.log(token);
+
+    // send the token in a HTTP-cookie
+    return _res
+      .cookie("token", token, {
+        httpOnly: true,
+      })
+      .send("Login was successful!");
+  } catch (err) {
+    console.error(err);
+    return _res.status(500).send("Wtf");
+  }
+});
+
+// logout
+router.get("/logout", (_req: Request, _res: Response) => {
+  _res
+    .cookie("token", "", {
+      httpOnly: true,
+      expires: new Date(0),
+    })
+    .send("logout");
 });
