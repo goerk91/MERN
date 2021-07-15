6fd10a3782fd7789b484f4f2470d9ede0ad81e06
Metin Görken <metin.goerken@adesso.de>
2021-07-15 17:38:11 +0200

    1) Added post and get router 2)middleware

--- routers/customerRouter.ts
+++ routers/customerRouter.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response } from "express";
 import { Customer } from "../models/customerModel";
+import auth from "../middleware/auth";
 
 const customerRouter = express.Router();
 
-customerRouter.post("/", async (_req: Request, _res: Response) => {
+customerRouter.post("/", auth, async (_req: Request, _res: Response) => {
   try {
     const { name } = _req.body;
 
@@ -13,6 +14,18 @@ customerRouter.post("/", async (_req: Request, _res: Response) => {
 
     const saveCustomer = await newCustomer.save();
     return _res.json(saveCustomer);
+  } catch (err) {
+    console.error(err);
+    return _res.status(500).send();
+  }
+});
+
+customerRouter.get("/", auth, async (_req: Request, _res: Response) => {
+  try {
+    //find gives all Customers
+    const allCustomer = await Customer.find();
+
+    return _res.json(allCustomer);
   } catch (err) {
     console.error(err);
     return _res.status(500).send();
